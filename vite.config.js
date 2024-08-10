f253f03566c213d87bec45675a01b7d0e4971105
sarif0605 <rivzalchantara13@gmail.com>
2024-08-10 19:33:59 +0700

    ganti link

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     proxy: {
       "/api": {
         target:
-          "https://splendid-loutitia-pelatihan-superbootcamp-1b641c17.koyeb.app/api",
+          "https://splendid-loutitia-pelatihan-superbootcamp-1b641c17.koyeb.app",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
